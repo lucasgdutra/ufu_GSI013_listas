0919d013b6658ad1b601db416deca79300a39f24
Henriquecostaf <72235744+Henriquecostaf@users.noreply.github.com>
2023-04-07 01:10:43 -0300

    Update 1.asm

--- lista_02/1.asm
+++ lista_02/1.asm
@@ -5,3 +5,56 @@
 #Utilize um laço para preencher o vetor e faça a impressão por meio de um procedimento.
 #O programa será executado 3 vezes e cada vez que for executado deverá escolher  3 posições diferentes do vetor para ser preenchida.
 
+.data
+ myArray:
+.align 2
+.space 84
+
+.text
+move $t0,$zero #indice do array
+move $t1,$zero #valor a ser alocado no array]
+li,$t2,84 #tamanho do array
+
+li $v0,5 #Lê inteiro
+syscall
+move $t3,$v0 #inteiro está em t3
+
+loop:
+beq $t0,$t2, saiDoLoop
+sw $t1, myArray($t0)
+addi $t0,$t0,4
+addi $t1,$t1,0
+j loop
+
+saiDoLoop:
+
+move $t0,$zero
+imprime:
+beq $t0,$t2, saiDaImpressao
+li,$v0,1
+lw $a0,myArray($t0)
+syscall
+
+addi $t0,$t0,4
+j imprime
+
+saiDaImpressao:
+
+bne $t0,$zero, labelPar
+beq $t0,$zero, labelImpar
+
+labelPar:
+
+labelImpar:
+
+move $t0,$zero
+imprime:
+beq $t0,$t2, saiDaImpressao
+li,$v0,1
+lw $a0,myArray($t0)
+syscall
+
+addi $t0,$t0,4
+j imprime
+
+saiDaImpressao:
